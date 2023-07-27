aba27dc16ac079fea663551a4825fb2918cde68b
MdAdnanMorshed <adnanmorshed118@gmail.com>
2023-07-27 22:49:56 +0600

    const added

--- lib/cubit/fetchdata_cubit.dart
+++ lib/cubit/fetchdata_cubit.dart
@@ -1,15 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
-
 import '../models/get_post_model.dart';
 import '../repositories/repo.dart';
-
 part 'fetchdata_state.dart';
-
 class FetchDataCubit extends Cubit<FetchDataState> {
   Repo repo;
   FetchDataCubit(this.repo) : super(FetchDataInitial());
-
   Future<void> fetchPost()async{
     try{emit(FetchDataLoading());
     final data=  await repo.fetchData();
